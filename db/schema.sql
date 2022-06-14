774e3a7676afba58fc96c0c4f07ff8facccb084b
Edgar Mendez <edgar.ym26@gmail.com>
2022-06-14 00:52:55 -0400

    fixing tables

--- db/schema.sql
+++ db/schema.sql
@@ -3,19 +3,17 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE departments(
+CREATE TABLE departments (
 
 dep_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-dep_name VARCHAR(30) NOT NULL,
-manager VARCHAR(30) NOT NULL
-
-    
+dep_name VARCHAR(30) NOT NULL  
 );
 
 
-CREATE TABLE roles(
-
-role_name VARCHAR(30) NOT NULL,
+CREATE TABLE roles (
+role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(30) NOT NULL,
+role_dep VARCHAR(30) NOT NULL,
 salary INT NOT NULL
     
 );
@@ -25,10 +23,9 @@ CREATE TABLE employees (
 empl_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 empl_first VARCHAR(30) NOT NULL,
 empl_last VARCHAR(30) NOT NULL,
-empl_role VARCHAR(30) NOT NULL, FOREIGN KEY (empl_role) REFERENCES roles(role_name),
+empl_title VARCHAR(30) NOT NULL, FOREIGN KEY (empl_title) REFERENCES roles(title),
 empl_dep VARCHAR(30) NOT NULL, FOREIGN KEY (empl_dep) REFERENCES departments(dep_name),
 empl_sal INT NOT NULL, FOREIGN KEY (empl_sal) REFERENCES roles(salary),
-is_manager BOOLEAN NOT NULL
-
+is_manager VARCHAR(30)
 
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,27 +1,28 @@
-INSERT INTO departments(dep_name,manager)
-VALUES  (sales, ),
-        (engineering, ),
-        (finance, ),
-        (legal, )
+INSERT INTO departments(dep_name,)
+VALUES  ("sales"),
+        ("engineering"),
+        ("finance"),
+        ("legal")
+                    ;
 
 
+INSERT INTO roles(role_name,salary)
+VALUES  ("sales lead",100,000),
+        ("salesperson",80,000),
+        ("lead engineer",150,000),
+        ("software engineer",120,000),
+        ("account manager",160,000),
+        ("accountant",125,000),
+        ("legal team lead",250,000),
+        ("lawyer",190,000)
         ;
 
 
-INSERT INTO ()
-VALUES  (),
-        (),
-        (),
-
-
-
-        ;
-
-
-INSERT INTO ()
-VALUES  (),
-        (),
-        (),
+INSERT INTO employees(empl_first,empl_last)
+VALUES  ("Stephen","Curry",),
+        ("Andre","Iguodala",),
+        ("Klay","Thompson",),
+        ("Draymond","Green",),
 
 
         ;
