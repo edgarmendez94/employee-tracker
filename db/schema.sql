8ebcdd9a230ec56902ed7acc67a02c774cc3c647
Edgar Mendez <edgar.ym26@gmail.com>
2022-06-13 22:26:19 -0400

    trying to finish up schema

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,13 @@ USE employee_db;
 
 CREATE TABLE employees (
 
-id INT NOT NULL PRIMARY KEY,
+empl_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+empl_first VARCHAR(30) NOT NULL,
+empl_last VARCHAR(30) NOT NULL,
+empl_role VARCHAR(30) NOT NULL, FOREIGN KEY (empl_role) REFERENCES roles(role_name),
+empl_dep VARCHAR(30) NOT NULL, FOREIGN KEY (empl_dep) REFERENCES departments(dep_name),
+emp_sal INT NOT NULL, FOREIGN KEY (empl_sal) REFERENCES roles(salary),
+is_manager BOOLEAN NOT NULL
 
 
 );
@@ -14,14 +20,18 @@ id INT NOT NULL PRIMARY KEY,
 
 CREATE TABLE departments (
 
+dep_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+dep_name VARCHAR(30) NOT NULL,
+manager VARCHAR(30) NOT NULL
 
     
 );
 
 
 CREATE TABLE roles (
-    
 
+role_name VARCHAR(30) NOT NULL,
+salary INT NOT NULL
     
 );
 
