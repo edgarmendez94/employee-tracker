37d5485f5e0ecb7b6271563d70b51e9651107b7f
Edgar Mendez <edgar.ym26@gmail.com>
2022-06-14 21:41:01 -0400

    finished menu

--- index.js
+++ index.js
@@ -6,16 +6,42 @@ function startApp() {
 inquirer
   .prompt([
     { type:'list',
-    name: 'start',
+    name: 'menu',
     message: 'What would you like to do ? ',
-    choices: ["View all employees","Add employee","Update employee role","View all roles","Add role","View all departments","Add department"] 
+    choices: ["View all employees","Add employee","Update employee role","View all roles","Add role","View all departments","Add department","Exit"] 
     },  
 
-
-
-
 ])
+.then((data) =>{
+    switch(data.menu){
+        case "View all employees":
+            getEmployees();
+            break;
+        case "Add employee":
+            addEmployee();
+            break;
+        case "Update employee role":
+            updateEmployee();
+            break;
+        case "View all roles":
+            getRoles();
+            break;
+        case "Add role":
+            addRole();
+            break;
+        case "View all departments":
+            getDepartments();
+            break;
+        case "Add department":
+            addDepartment();
+            break;
+        case "Exit":
+            process.exit();
+            break;
 
+    }
+
+});
 }
 
 
