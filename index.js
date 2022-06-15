d7b341ce23198f38e2d2e2f1844514758797e570
Edgar Mendez <edgar.ym26@gmail.com>
2022-06-14 21:16:22 -0400

    js stuff now

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 const inquirer = require('inquirer');
+const mysql = require("mysql2");
 
+function startApp() {
 
 inquirer
   .prompt([
-    {
-      message: '',
-      name: '',
+    { type:'list',
+    name: 'start',
+    message: 'What would you like to do ? ',
+    choices: ["View all employees","Add employee","Update employee role","View all roles","Add role","View all departments","Add department"] 
     },  
 
 
 
 
 ])
+
+}
+
+
+
+
+
+// view all departments 
+    // select * from departments;
+
+
+function getDepartments() {
+    db.query("select * from departments")
+    .then((results) => {
+        return results;
+    })
+}
+
+// view all employees
+    // select * from employees;
+
+
+// view all roles
+    // select * from roles;
